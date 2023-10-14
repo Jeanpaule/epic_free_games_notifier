896bfb3c901f7dc75a071af657e0b4c903422ff7
Jeanpaule <jeanpg82@gmail.com>
2023-10-14 11:11:36 -0500

    Initial commit

---
+++ lib/epic_free_games_notifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "epic_free_games_notifier/version"
+
+module EpicFreeGamesNotifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
